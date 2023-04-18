7c093c03444c2018762e6a988904eeabf168f4c6
Jin <jinwoodjin@gmail.com>
2023-04-18 13:32:24 -0700

    Add src

---
+++ src/contact.js
@@ -0,0 +1,28 @@
+function createContact() {
+    const contact = document.createElement('div')
+    contact.classList.add('contact')
+  
+    const phoneNumber = document.createElement('p')
+    phoneNumber.textContent = '📞 123 456 789'
+  
+    const address = document.createElement('p')
+    address.textContent = '🏠 1 E Cordova St #130, Vancouver, Canada'
+  
+    const restaurantLocation = document.createElement('img')
+    restaurantLocation.src = 'images/restaurant-location.png'
+    restaurantLocation.alt = 'Burgermania restaurant location'
+  
+    contact.appendChild(phoneNumber)
+    contact.appendChild(address)
+    contact.appendChild(restaurantLocation)
+  
+    return contact
+  }
+  
+  function loadContact() {
+    const main = document.getElementById('main')
+    main.textContent = ''
+    main.appendChild(createContact())
+  }
+  
+  export default loadContact
---
+++ src/menu.js
@@ -0,0 +1,75 @@
+function createMenu() {
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+  
+    menu.appendChild(
+      createMenuItem(
+        "Beef Burger",
+        "Beef patty, Lettuce, Onion, Tomato, Pickle, Cheese, Mayo, Ketchup"
+      )
+    );
+    menu.appendChild(
+      createMenuItem(
+        "Cheddar Burger",
+        "Beef patty, Cheddar, Tomato, Letuce"
+      )
+    );
+    menu.appendChild(
+      createMenuItem(
+        "Chicken Burger",
+        "Chicken, Tomato, Lettuce, Mayo"
+      )
+    );
+    menu.appendChild(
+      createMenuItem(
+        "Deluxe Burger",
+        "Beef patty, Cheese, Lettuce"
+      )
+    );
+    menu.appendChild(
+      createMenuItem(
+        "Egg Burger",
+        "Beef patty, Egg, Tomato, Lettuce"
+      )
+    );
+    menu.appendChild(
+      createMenuItem(
+        "Fish Burger",
+        "Fish patty, Cheese, Mayo"
+      )
+    );
+  
+    return menu;
+  }
+  
+  function createMenuItem(name, description) {
+    const menuItem = document.createElement("div");
+    menuItem.classList.add("menu-item");
+  
+    const foodName = document.createElement("h2");
+    foodName.textContent = name;
+  
+    const foodDescription = document.createElement("p");
+    foodDescription.textContent = description;
+  
+    const foodImage = document.createElement("img");
+    foodImage.src = `images/burgers/${name.toLowerCase()}.png`;
+    foodImage.alt = `${name}`;
+  
+    menuItem.appendChild(foodImage);
+    menuItem.appendChild(foodName);
+    menuItem.appendChild(foodDescription);
+  
+    return menuItem;
+  }
+  
+  function loadMenu() {
+    const main = document.getElementById("main");
+    main.textContent = "";
+    main.appendChild(createMenu());
+  }
+  
+  export default loadMenu;
+
+  
+  
